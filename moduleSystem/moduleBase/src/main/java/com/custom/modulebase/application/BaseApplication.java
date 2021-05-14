cb02f5571c9c92e0e8e2cc66f92fc9d37339bffd
linliming <13156865236@163.com>
2021-05-14 14:53:56 +0800

    LiveEventBus添加

--- moduleSystem/moduleBase/src/main/java/com/custom/modulebase/application/BaseApplication.java
+++ moduleSystem/moduleBase/src/main/java/com/custom/modulebase/application/BaseApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 
 import com.alibaba.android.arouter.launcher.ARouter;
 import com.custom.modulebase.BuildConfig;
+import com.jeremyliao.liveeventbus.LiveEventBus;
+import com.jeremyliao.liveeventbus.logger.DefaultLogger;
 
 public class BaseApplication extends Application {
 
@@ -16,5 +18,11 @@ public class BaseApplication extends Application {
             ARouter.openDebug();   // 开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！线上版本需要关闭,否则有安全风险)
         }
         ARouter.init(this); // 尽可能早，推荐在Application中初始化
+        LiveEventBus
+                .config()
+                .lifecycleObserverAlwaysActive(true)
+                .autoClear(true)
+                .enableLogger(true)
+                .setContext(getApplicationContext());
     }
 }
